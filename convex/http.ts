1c7ecaeeaaf44096e92faa2ac061752aee6593fc
Mubarek <mubirimuru@gmail.com>
2025-06-16 22:26:38 +0300

    webhook

--- convex/http.ts
+++ convex/http.ts
@@ -1,19 +1,18 @@
 import { httpRouter } from "convex/server";
-import { httpAction } from "./_generated/server";
 import { Webhook } from "svix";
 import { api } from "./_generated/api";
-
+import { httpAction } from "./_generated/server";
 const http = httpRouter();
 
 http.route({
-    path:"/clerk-webhook",
-    method:"POST",
-    handler: httpAction(async(ctx, request)=>{
+  path: "/clerk-webhook",
+  method: "POST",
+  handler: httpAction(async (ctx, request) => {
     const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
     if (!webhookSecret) {
-        throw new Error("Missing CLERK_WEBHOOK_SECRET environment variable");
-      }
-        // check headers
+      throw new Error("Missing CLERK_WEBHOOK_SECRET environment variable");
+    }
+    // check headers
     const svix_id = request.headers.get("svix-id");
     const svix_signature = request.headers.get("svix-signature");
     const svix_timestamp = request.headers.get("svix-timestamp");
@@ -30,15 +29,35 @@ http.route({
     let evt: any;
     //verify webhook
     try {
-        evt = wh.verify(body, {
-          "svix-id": svix_id,
-          "svix-timestamp": svix_timestamp,
-          "svix-signature": svix_signature,
-        }) as any;
-      } catch (err) {
-        console.error("Error verifying webhook:", err);
-        return new Response("Error occurred", { status: 400 });
+      evt = wh.verify(body, {
+        "svix-id": svix_id,
+        "svix-timestamp": svix_timestamp,
+        "svix-signature": svix_signature,
+      }) as any;
+    } catch (err) {
+      console.error("Error verifying webhook:", err);
+      return new Response("Error occurred", { status: 400 });
+    }
+    const eventType = evt.type;
+    if (eventType === "user.created") {
+      const { id, email_addresses, first_name, last_name, image_url } =
+        evt.data;
+      const email = email_addresses[0].email_address;
+      const name = `${first_name || ""} ${last_name || ""}`.trim();
+      try {
+        await ctx.runMutation(api.users.createUser, {
+          email,
+          fullname: name,
+          image: image_url,
+          clerkId: id,
+          username: email.split("@")[0],
+        });
+      } catch (error) {
+        console.log("Error creating user:", error);
+        return new Response("Error creating user", { status: 500 });
       }
-
-   })
-})
+    }
+    return new Response("Webhook processed successfully", { status: 200 });
+  }),
+});
+export default http;
