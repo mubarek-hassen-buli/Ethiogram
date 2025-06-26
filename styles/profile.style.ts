4eb018353f9309e8fc54fd0beb627b9ba9d991ce
Mubarek <mubirimuru@gmail.com>
2025-06-26 10:59:04 +0300

    profile screen

--- styles/profile.style.ts
+++ styles/profile.style.ts
@@ -46,8 +46,8 @@ export const styles = StyleSheet.create({
     marginRight: 32,
   },
   avatar: {
-    width: 86,
-    height: 86,
+    width: 60,
+    height: 60,
     borderRadius: 43,
     borderWidth: 2,
     borderColor: COLORS.surface,
